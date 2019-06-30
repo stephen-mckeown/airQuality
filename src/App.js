8ea7661c1f2ee69c72c2bd527e422fb6929651cf
Stephen McKeown <stephen.mckeown@sigma.se>
2019-06-30 15:37:46 +0100

    Added drop down list of filtered cities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import CitySearch from './components/CitySearch.js'
 
 function App() {
   return (
@@ -14,69 +15,3 @@ function App() {
 
 export default App;
 
-
-export class CitySearch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-            value: '',
-            cities: [],
-            filteredCites: []
-          };
-    this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidMount() {
-    fetch("https://api.openaq.org/v1/cities?limit=200&country=GB", { method: 'GET' })
-      .then(res => res.json())
-      .then(
-        (resultsObj) => {
-          console.log(resultsObj.results)
-          this.setState({
-            isLoaded: true,
-            cities: resultsObj.results
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-
-  handleChange(event) {
-    let cities = this.state.cities
-    let search = event.target.value
-
-    var regex = new RegExp('^' + search, 'i');
-    let filterCities = cities.filter((city, index) => {
-      return regex.test(city.city);
-    })
-    console.log(regex, "regex")
-    console.log(filterCities, "filterCities")
-    this.setState({
-      value: event.target.value,
-      filteredCites: filterCities,
-    });
-  }
-
-  // handleSubmit(event) {
-  //   alert('A name was submitted: ' + this.state.value);
-  //   event.preventDefault();
-  // }
-
-  render() {
-    return (
-        <label>
-        <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Enter city name..."  />
-        </label>
-    );
-  }
-}
---
+++ src/components/CitySearch.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import './CitySearch.css'
+
+class CitySearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      cities: [],
+      filteredCites: [],
+      open: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://api.openaq.org/v1/cities?limit=200&country=GB", { method: 'GET' })
+      .then(res => res.json())
+      .then(
+        (resultsObj) => {
+          console.log(resultsObj.results)
+          this.setState({
+            isLoaded: true,
+            cities: resultsObj.results
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
+  handleChange(event) {
+    let cities = this.state.cities
+    let search = event.target.value
+
+    var regex = new RegExp('^' + search, 'i');
+    let filterCities = cities.filter((city, index) => {
+      return regex.test(city.city);
+    })
+    console.log(regex, "regex")
+    console.log(filterCities, "filterCities")
+    this.setState({
+      value: event.target.value,
+      filteredCites: filterCities,
+      open: true
+    });
+  }
+
+  // handleSubmit(event) {
+  //   alert('A name was submitted: ' + this.state.value);
+  //   event.preventDefault();
+  // }
+
+  selectCity(id) {
+    console.log(id, "selectCity")
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input className="cityInput" type="text" value={this.state.value} onChange={this.handleChange} placeholder="Enter city name..." />
+        {this.state.open &&
+          <div className="dropdown">
+            <ul>
+              {this.state.filteredCites.map((city, index) => {
+                return <li key={index} onClick={() => this.selectCity(index)}>{city.city}</li>
+              })}
+            </ul>
+          </div>
+
+        }
+      </div>
+    );
+  }
+}
+
+export default CitySearch;
