4f173f7ce248b5c4d05f88fc86595ed0a7516039
Stephen McKeown <stephen.mckeown@sigma.se>
2019-06-30 08:29:26 +0100

    component 'CitySearch' added with text input

--- src/App.js
+++ src/App.js
@@ -4,9 +4,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Air quality</h1>
+      <h1>Compare your Air</h1>
+      <h2>Compare the air quality between cities in the UK.</h2>
+      <h2>Select cities to compare using the search tool below.</h2>
+      <CitySearch/>
     </div>
   );
 }
 
 export default App;
+
+
+export class CitySearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  // handleSubmit(event) {
+  //   alert('A name was submitted: ' + this.state.value);
+  //   event.preventDefault();
+  // }
+
+  render() {
+    return (
+        <label>
+        <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Enter city name..."  />
+        </label>
+    );
+  }
+}
