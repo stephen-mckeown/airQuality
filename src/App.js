235db1b882f55267110eceb1335e27c4266d758e
Stephen McKeown <stephen.mckeown@sigma.se>
2019-07-03 15:53:44 +0100

    styling added to background + addition elements

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import CitySearch from './components/CitySearch.js'
 function App() {
   return (
     <div className="App">
-      <h1>Compare your Air</h1>
-      <h2>Compare the air quality between cities in the UK.</h2>
-      <h2>Select cities to compare using the search tool below.</h2>
+      <div className="mainFont">
+        <h1>Compare your Air</h1>
+        <h2>Compare the air quality between cities in the UK.</h2>
+        <h2>Select cities to compare using the search tool below.</h2>
+      </div>
       <CitySearch/>
     </div>
   );
